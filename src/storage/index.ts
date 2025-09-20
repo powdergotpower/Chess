d342b92a578223e4e7475f0fcf863ab48052ef4e
powdergotpower <anshjotsinghsidhu@gmail.com>
2025-09-20 14:14:16 +0530

    Delete src directory

--- src/storage/index.ts
+++
@@ -1,7 +0,0 @@
-import { PostgresStore } from "@mastra/pg";
-
-// Create a single shared PostgreSQL storage instance
-export const sharedPostgresStorage = new PostgresStore({
-  connectionString:
-    process.env.DATABASE_URL || "postgresql://localhost:5432/mastra",
-});
